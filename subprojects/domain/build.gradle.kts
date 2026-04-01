ef49f60ba32e628e1d1a8369ad912a153f899299
qkrwlgus5832 <pjh_5890@naver.com>
2026-04-02 03:55:35 +0900

    H2 지우기

--- subprojects/domain/build.gradle.kts
+++ subprojects/domain/build.gradle.kts
@@ -4,7 +4,6 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     api("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta")
     kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
-    runtimeOnly("com.h2database:h2")
     implementation("mysql:mysql-connector-java:8.0.33")
     implementation(kotlin("stdlib-jdk8"))
 }
